5341dec85b40c19887be58d21fbc3839e89be010
zrll_ <zhengruilin0@126.com>
2024-05-20 04:30:52 +0800

    fix: adjust layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {Button, Center, Group, Space, Stack, Table, ScrollArea, Box} from '@mantine/core';
+import { Button, Center, Group, Space, Stack, Table, ScrollArea, Box } from '@mantine/core';
 import { useState } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
 import FileRow from '@/components/FileRow/FileRow';
@@ -29,52 +29,52 @@ export default function HomePage() {
 
     return (
         <>
-            <Center>
-                <Stack>
-                    <Space />
+            <Stack>
+                <Space />
+                <Center>
                     <Group>
                         <Button onClick={selectFile}>{md5file === '' ? 'Open file' : `MD5 file: ${md5file}`}</Button>
                         {md5file !== '' &&
                             <Button
                               variant="subtle"
                               onClick={() => {
-                                setMD5File('');
-                                setCheckMode(false);
-                              }}>Close File
+                                    setMD5File('');
+                                    setCheckMode(false);
+                                }}>Close File
                             </Button>}
                         {md5file === '' &&
                             <Button variant="light" onClick={addNewFile}>Add new file</Button>}
                     </Group>
+                </Center>
 
-                    <ScrollArea w={800}>
-                        <Box w={1200}>
-                            <Table>
-                                <Table.Thead>
-                                    <Table.Tr>
-                                        <Table.Th>File Name</Table.Th>
-                                        <Table.Th>Size</Table.Th>
-                                        <Table.Th>Progress</Table.Th>
-                                        <Table.Th></Table.Th>
-                                        <Table.Th>Hash</Table.Th>
-                                        <Table.Th>Record</Table.Th>
-                                    </Table.Tr>
-                                </Table.Thead>
-                                <Table.Tbody>
-                                    {files.map((file, index) =>
-                                        <FileRow
-                                            checkMode
-                                            file={file}
-                                            onDelete={() => {
-                                                const deleted_file = [...files];
-                                                deleted_file.splice(index, 1);
-                                                setFiles(deleted_file);
-                                            }} />)}
-                                </Table.Tbody>
-                            </Table>
-                        </Box>
-                    </ScrollArea>
-                </Stack>
-            </Center>
+                <ScrollArea w={800}>
+                    <Box w={1200}>
+                        <Table>
+                            <Table.Thead>
+                                <Table.Tr>
+                                    <Table.Th>File Name</Table.Th>
+                                    <Table.Th>Size</Table.Th>
+                                    <Table.Th>Progress</Table.Th>
+                                    <Table.Th></Table.Th>
+                                    <Table.Th>Hash</Table.Th>
+                                    <Table.Th>Record</Table.Th>
+                                </Table.Tr>
+                            </Table.Thead>
+                            <Table.Tbody>
+                                {files.map((file, index) =>
+                                    <FileRow
+                                      checkMode
+                                      file={file}
+                                      onDelete={() => {
+                                            const deleted_file = [...files];
+                                            deleted_file.splice(index, 1);
+                                            setFiles(deleted_file);
+                                        }} />)}
+                            </Table.Tbody>
+                        </Table>
+                    </Box>
+                </ScrollArea>
+            </Stack>
         </>
     );
 }
